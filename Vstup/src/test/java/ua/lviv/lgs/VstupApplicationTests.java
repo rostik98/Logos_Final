743c041783f13d4b4f467da92db1a6e2137a1a90
rostikrff98 <sheremeta967@ukr.net>
2021-03-20 14:52:21 +0200

    fix problem with ComponentScan and add tests for all Services

--- Vstup/src/test/java/ua/lviv/lgs/VstupApplicationTests.java
+++ Vstup/src/test/java/ua/lviv/lgs/VstupApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ua.lviv.lgs.dao.EnrolledRepository;
@@ -15,7 +16,11 @@ import ua.lviv.lgs.dao.EntrantRepository;
 import ua.lviv.lgs.dao.FacultyRepository;
 import ua.lviv.lgs.dao.MarkRepository;
 import ua.lviv.lgs.dao.SubjectRepository;
+import ua.lviv.lgs.domain.Enrolled;
 import ua.lviv.lgs.domain.Entrant;
+import ua.lviv.lgs.domain.Faculty;
+import ua.lviv.lgs.domain.Mark;
+import ua.lviv.lgs.domain.Subject;
 import ua.lviv.lgs.service.EnrolledService;
 import ua.lviv.lgs.service.EntrantService;
 import ua.lviv.lgs.service.FacultyService;
@@ -24,6 +29,7 @@ import ua.lviv.lgs.service.SubjectService;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ComponentScan("ua.lviv.lgs")
 class VstupApplicationTests {
 
 	@Autowired
@@ -69,4 +75,141 @@ class VstupApplicationTests {
 		assertTrue(entrantFromDb.getLastName().equals(entrant.getLastName()));
 	}
 
+	@Test
+	public void testFindByEmail() {
+		List<Entrant> entrants = entrantRepository.findAll();
+		assertTrue(entrants.size() == 0);
+		Entrant entrant = new Entrant();
+		entrant.setEmail("1@gmail.com");
+		entrant.setFirstName("1");
+		entrant.setLastName("1");
+		entrant.setPassword("1");
+		entrant.setPasswordConfirm("1");
+
+		entrantRepository.save(entrant);
+
+		Entrant findByEmail = entrantService.getOneByEmail(entrant.getEmail());
+
+		assertTrue(findByEmail.getEmail().equals(entrant.getEmail()));
+	}
+
+	@Test
+	public void testGetAllEntrants() {
+		Entrant entrant = new Entrant();
+		entrant.setEmail("1@gmail.com");
+		entrant.setFirstName("1");
+		entrant.setLastName("1");
+		entrant.setPassword("1");
+		entrant.setPasswordConfirm("1");
+
+		entrantRepository.save(entrant);
+
+		Entrant entrant1 = new Entrant();
+		entrant1.setEmail("2@gmail.com");
+		entrant1.setFirstName("2");
+		entrant1.setLastName("2");
+		entrant1.setPassword("2");
+		entrant1.setPasswordConfirm("2");
+
+		entrantRepository.save(entrant1);
+		List<Entrant> entrants = entrantRepository.findAll();
+		assertTrue(entrants.size() == 2);
+	}
+
+	@Test
+	public void testSubject() {
+		List<Subject> subjects = subjectRepository.findAll();
+		assertTrue(subjects.size() == 0);
+
+		Subject subject = new Subject();
+		subject.setName("Physic");
+
+		subjectRepository.save(subject);
+
+		subjects = subjectService.getAllSubjects();
+		assertTrue(subjects.size() == 1);
+		Subject findById = subjectService.findById(subjects.get(0).getId());
+		assertTrue(findById.getName() == "Physic");
+
+	}
+
+	@Test
+	public void testMark() {
+		Entrant entrant = new Entrant();
+		entrant.setEmail("1@gmail.com");
+		entrant.setFirstName("1");
+		entrant.setLastName("1");
+		entrant.setPassword("1");
+		entrant.setPasswordConfirm("1");
+		Mark mark = new Mark();
+		mark.setValue(100);
+		mark.setEntrant(entrant);
+		Subject subject = new Subject();
+		subject.setName("Physic");
+		mark.setSubject(subject);
+		subjectRepository.save(subject);
+		entrantRepository.save(entrant);
+		markRepository.save(mark);
+		assertTrue(markService.getAllMarks().get(0).getEntrant().getPassword().equals("1"));
+		assertTrue(markService.getAllMarks().get(0).getSubject().getName().equals("Physic"));
+	}
+
+	@Test
+	public void testFaculty() {
+		Faculty faculty = new Faculty();
+		faculty.setId(1);
+		faculty.setName("QWERTY");
+		faculty.setBudgetPlaces(10);
+		faculty.setPassingScore(5.);
+		facultyRepository.save(faculty);
+
+		assertTrue(facultyService.findByName("QWERTY").getPassingScore() == 5.);
+	}
+
+	@Test
+	public void testEnrolled() {
+		Entrant entrant = new Entrant();
+		entrant.setEmail("1@gmail.com");
+		entrant.setFirstName("1");
+		entrant.setLastName("1");
+		entrant.setPassword("1");
+		entrant.setPasswordConfirm("1");
+
+		entrantRepository.save(entrant);
+
+		Mark mark = new Mark();
+		mark.setValue(100);
+		mark.setEntrant(entrant);
+		Subject subject = new Subject();
+		subject.setName("Physic");
+		subjectRepository.save(subject);
+		mark.setSubject(subject);
+		markRepository.save(mark);
+
+		Mark mark1 = new Mark();
+		mark1.setValue(150);
+		mark1.setEntrant(entrant);
+		Subject subject1 = new Subject();
+		subject1.setName("Physic1");
+		subjectRepository.save(subject1);
+		mark1.setSubject(subject1);
+		markRepository.save(mark1);
+
+		Faculty faculty = new Faculty();
+		faculty.setId(1);
+		faculty.setName("QWERTY");
+		faculty.setBudgetPlaces(10);
+		faculty.setPassingScore(180.);
+
+		facultyRepository.save(faculty);
+
+		Enrolled enrolled = new Enrolled();
+		enrolled.setFacultyId(faculty);
+		enrolled.setEntrantId(entrant);
+		enrolled.setCompetitiveScore((mark.getValue() + mark1.getValue()) / 2.);
+		if (enrolled.getFacultyId().getPassingScore() <= enrolled.getCompetitiveScore()) {
+			enrolledService.save(enrolled);
+		}
+		assertTrue(enrolledRepository.findAll().size() == 0);
+	}
 }
